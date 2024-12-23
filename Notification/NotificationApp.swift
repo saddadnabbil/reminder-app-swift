44d73594208398d1fa8bcd76ca70eb0e651f3a18
saddadnabbil <saddadnabbil@gmail.com>
2024-12-23 11:54:10 +0700

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Saddad Nabbil on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotificationTests/NotificationTests.swift
@@ -0,0 +1,16 @@
+//
+//  NotificationTests.swift
+//  NotificationTests
+//
+//  Created by Saddad Nabbil on 23/12/24.
+//
+
+import Testing
+
+struct NotificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
